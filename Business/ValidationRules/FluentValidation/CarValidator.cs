3a25a20a5bca2eb147ff666c12cb8c32999e6969
busranurbaydur <busranurbaydur@outlook.com>
2021-10-06 16:44:45 +0300

    Validationrules Added

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0).WithMessage("Günlük fiyat 0'dan büyük olmalıdır.");
+        }
+    }
+}
